c3d9472625877bb58a1b35717ae591e7be0ab60d
Semkiv Andriy <semkivandriy@gmail.com>
2023-07-02 10:14:18 +0300

    deleted unused actions

--- FolderTree/Controllers/FoldersController.cs
+++ FolderTree/Controllers/FoldersController.cs
@@ -36,10 +36,13 @@ namespace FolderTree.Controllers
 				return NotFound();
 			}
 
-            List<Folder> subFoldersList = await _context.Folders.Where(f => f.ParrentId == id).ToListAsync();
+            List<Folder> subFoldersList = await _context.Folders
+                .Where(f => f.ParrentId == id)
+                .ToListAsync();
 
 			var folder = await _context.Folders
 				.FirstOrDefaultAsync(m => m.Id == id);
+
 			if (folder == null)
 			{
 				return NotFound();
@@ -52,16 +55,5 @@ namespace FolderTree.Controllers
 			return View(folder);
         }
 
-        // GET: Folders
-        public async Task<IActionResult> Index()
-        {
-            var appDbContext = _context.Folders.Include(f => f.ParrentFolder);
-            return View(await appDbContext.ToListAsync());
-        }
-
-        private bool FolderExists(int id)
-        {
-          return (_context.Folders?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
 }
