58beb437f141d9b347f9dcda312a08f30d80e458
Semkiv Andriy <semkivandriy@gmail.com>
2023-07-02 10:10:32 +0300

    upd appdbcontext

--- FolderTree/Models/AppDbContext.cs
+++ FolderTree/Models/AppDbContext.cs
@@ -8,7 +8,6 @@ namespace FolderTree.Models
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            //Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
